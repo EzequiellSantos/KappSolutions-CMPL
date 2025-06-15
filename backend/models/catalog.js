d731fc512d9ae94feddf7bc9115315086e38f437
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2025-06-15 13:26:22 -0300

    Add Catalog Model

---
+++ backend/models/catalog.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ProductSummarySchema = new mongoose.Schema({
+    code: String,
+    description: String,
+    price: Number
+    // Adicionar outros campos resumidos do produto conforme necessário
+})
+
+const CatalogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+    },
+    materials: {
+        type: [ProductSummarySchema],
+    },
+    totalValues: {
+        type: Number,
+        required: true 
+    },
+    idCompany: {
+        type: String, 
+        required: true 
+    }
+});
+
+module.exports = mongoose.model('Catalog', CatalogSchema);
---
+++ backend/routes/catalogRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const Catalog = require('../models/catalog')
+
+// @desc    fazer as quatros rotas de CRUD para o Catálogo com base nas regras
+
+
+
+module.exports = router
